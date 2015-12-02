b4a6f1cb5b747a9552337ec91423bbffea8fb550
Clay Freeman <git@clayfreeman.com>
2015-12-01 23:01:41 -0600

    add license

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
  * sandbox directory mimicking an extremely basic subset of the HTTP version
  * 1.0 protocol in a secure manner
  *
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0
+ * International License. To view a copy of this license, visit:
+ * http://creativecommons.org/licenses/by-sa/4.0/
+ *
  * @author     Clay Freeman
  * @date       November 30, 2015
  */
